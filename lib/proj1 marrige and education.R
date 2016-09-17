2d66495c2d92e8d1d6cc1ce7fec80a0586ef36ee
Nicole Jiang <cj2451@columbia.edu>
2016-09-16 23:41:37 -0400

    Update proj1 marrige and education.R

--- lib/proj1 marrige and education.R
+++ lib/proj1 marrige and education.R
@@ -1,7 +1,7 @@
 library(data.table)
 colstokeep_pus <- c("AGEP", "MAR", "SCHL", "FOD1P", "SCIENGRLP")
-dataset_a <- fread('csv_pus/ss14pusa.csv', select= colstokeep)
-dataset_b <- fread('csv_pus/ss14pusb.csv', select= colstokeep)
+dataset_a <- fread('csv_pus/ss14pusa.csv', select= colstokeep_pus)
+dataset_b <- fread('csv_pus/ss14pusb.csv', select= colstokeep_pus)
 data.fm<- data.frame(rbind(dataset_a, dataset_b)[AGEP >21,])
 
 #names(data.fm)
