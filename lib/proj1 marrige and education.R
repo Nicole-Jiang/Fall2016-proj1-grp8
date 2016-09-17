cb54097aa8a0dc2a6ef62adf8eb145e66ae24319
Nicole Jiang <cj2451@columbia.edu>
2016-09-16 23:53:02 -0400

    Update proj1 marrige and education.R

--- lib/proj1 marrige and education.R
+++ lib/proj1 marrige and education.R
@@ -2,7 +2,9 @@ library(data.table)
 colstokeep_pus <- c("AGEP", "MAR", "SCHL", "FOD1P", "SCIENGRLP")
 dataset_a <- fread('csv_pus/ss14pusa.csv', select= colstokeep_pus)
 dataset_b <- fread('csv_pus/ss14pusb.csv', select= colstokeep_pus)
-data.fm<- data.frame(rbind(dataset_a, dataset_b)[AGEP >21,])
+data.fm<- data.frame(rbind(dataset_a, dataset_b)[AGEP >18,])
 
 #names(data.fm)
 hist(data.fm[,2])
+
+> nrow(subset(data.fm, MAR == 5)) / nrow(data.fm) # never married people perportion
