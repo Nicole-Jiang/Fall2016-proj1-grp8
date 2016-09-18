f553ea13f03df07b62ee738b269c4778f1b1076a
Nicole Jiang <cj2451@columbia.edu>
2016-09-18 00:03:40 -0400

    marry and edu figure

--- lib/proj1 marrige and education.R
+++ lib/proj1 marrige and education.R
@@ -1,52 +1,88 @@
-# data cleansing
+#setwd("~/Documents/Courses/ADS/Project 1")
+# libraries we need
+# install necessary package
+#install.packages('devtools')
+#install.packages("FactoMineR")
+#install.packages("gplots")
+#devtools::install_github("kassambara/factoextra")
+library("FactoMineR")
+library("factoextra")
+library("gplots")
 library(data.table)
+
+# data cleansing
 colstokeep_pus <- c("AGEP", "MAR", "MARHT", "SCHL", "FOD1P", "SCIENGRLP")
 dataset_a <- fread('csv_pus/ss14pusa.csv', select= colstokeep_pus)
 dataset_b <- fread('csv_pus/ss14pusb.csv', select= colstokeep_pus)
 data.fm<- data.frame(rbind(dataset_a, dataset_b)[AGEP >18,])
 data.fm[is.na(data.fm)] <- 0
 
-#names(data.fm)
-#hist(data.fm[,3])
 #nrow(subset(data.fm, MAR == 5)) / nrow(data.fm) # never married people perportion
 
-# install necessary package
-#install.packages('devtools')
-#install.packages("FactoMineR")
-#install.packages("gplots")
-
-# libraries we need
-library("FactoMineR")
-library("factoextra")
-library("gplots")
-devtools::install_github("kassambara/factoextra")
-
+#####################################################
 # education level calculation
 #single_lowedu= nrow(subset(data.fm, MAR==5&SCHL<15))
 #single_highschool= nrow(subset(data.fm, MAR==5 & SCHL> 16& SCHL< 20))
 #single_bachelor = 
-lowedu= subset(data.fm, SCHL<15) #lower than high school
-highschool= subset(data.fm, SCHL> 16 & SCHL< 20) #including high school, GED, Associate's degree or enter the college without degree
+lowedu= subset(data.fm, SCHL<=15) #lower than high school
+highschool= subset(data.fm, SCHL>= 16 & SCHL<= 20) #including high school, GED, Associate's degree or enter the college without degree
 bachelor= subset(data.fm, SCHL==21) 
 master= subset(data.fm, SCHL==22| SCHL ==23) #including master and professional degree
 phd= subset(data.fm, SCHL==24)
 #dim(master)
 data_edu= list(lowedu,highschool,bachelor, master, phd)
 
+#####################################################
 #generate marry-education matrix
 mar_edu= matrix(nrow=5,ncol=4)
 rownames(mar_edu) <- c("Low education", "High school", "Bachelor", "Master", "PhD")
-colnames(mar_edu) <- c("Never married", "Once", "Twice", "More than 3 times")
+colnames(mar_edu) <- c("Never", "Once", "Twice", ">3 times")
 for(i in 1:5){
   for(j in 1:4){
     mar_edu[i,j]= nrow(subset(data_edu[[i]],MARHT == j-1))
   }
 }
 
-balloonplot(t(as.table(mar_edu)), main ="marry and edu", xlab ="", ylab="", label = FALSE, show.margins = FALSE)
+#####################################################
+#draw the balloonplot
+balloonplot(t(as.table(mar_edu)), main ="Marriage and Education level", xlab ="Marriage", ylab="Education level",
+             dotsize = 6, text.size= 0.7, label = FALSE, show.margins = FALSE)
+#http://www.sthda.com/english/wiki/correspondence-analysis-in-r-the-ultimate-guide-for-the-analysis-
+#the-visualization-and-the-interpretation-r-software-and-data-mining#at_pco=smlwn-1.0&at_si=57dd7e521
+#65dfc04&at_ab=per-2&at_pos=0&at_tot=1
 
+#####################################################
+#draw the sunburst plot
+#devtools::install_github
+#devtools::install_github("timelyportfolio/sunburstR")
+library(sunburstR)
+library(TraMineR)
+library(pipeR)
 
+edu= subset(data.fm, select= c("MARHT","SCHL"))
+edu_test= edu[1:10000,] ### 数据量太大，先跑10000行，之后再说吧
 
-
-
+for(i in 1:nrow(edu_test)){
+  if(edu_test[i,2]<15){
+    edu_test[i,2]="L"
+  }else if(edu_test[i,2]>=16 & edu_test[i,2]<=20){
+    edu_test[i,2]="H"
+  }else if(edu_test[i,2]==21){
+    edu_test[i,2]="B"
+  }else if(edu_test[i,2]==22|edu_test[i,2]==23){
+    edu_test[i,2]="M"
+  }else{
+    edu_test[i,2]="P"
+  }
+  #if(i%%1000==0){
+  #  print(i)
+  #}
+}
+#prepare for sunburst plot
+edu.seq <- seqdef(edu_test)
+sun_edu=seqtab(edu.seq, tlim = 0, format = "STS" )
+name=names(attributes(as.list(sun_edu))$weights)
+freq=attributes(as.list(sun_edu))$freq[,2]
+sun_edu.fm=data.frame(name,freq)
+sunburst(sun_edu.fm)
 
