7a1a964e5dcc2830193b141af3e6760c6d0daa80
Minghao Dai <md3397@columbia.edu>
2016-09-19 00:52:17 -0400

    update

--- lib/marraige_worktime.R
+++ lib/marraige_worktime.R
@@ -2,11 +2,13 @@ library(data.table)
 library(plyr)
 library(ggplot2)
 
-colstokeep_time <- c("MARHD", "MARHM", "MARHT", "JWAP", "JWDP", "SEX", "AEGP")
-T1 <- fread('ss13pusa.csv', select = colstokeep_time, na.strings = "bbb")
-T2 <- fread('ss13pusb.csv', select = colstokeep_time, na.strings = "bbb")
+colstokeep_time <- c("MARHD", "MARHM", "MARHT", "JWAP", "JWDP", "SEX", "AEGP", "PERNP")
+T1 <- fread('ss13pusa.csv', select = colstokeep_time, na.strings = c("bbb", "bbbbbbb"))
+T2 <- fread('ss13pusb.csv', select = colstokeep_time, na.strings = c("bbb", "bbbbbbb"))
 Tfull <- rbind(T1, T2)
 Tuse <- na.omit(Tfull)
+
+Tuse$SEX <- as.factor(Tuse$SEX)
 
 Tuse$JWAP_h <- ifelse(Tuse$JWAP %in% c(1:10), 0, 
                       ifelse(Tuse$JWAP %in% c(11,21), 1, 
@@ -25,29 +27,60 @@ findrate <- function(x) sum(x==1)/length(x)
 rate_arr <- ddply(Tuse, .(JWAP_h, SEX), summarize, 
             n = length(MARHD), drate = findrate(MARHD), mrate = findrate(MARHM))
 
-rate_dep <- ddply(Tuse, .(JWDP_h), summarize, 
-            n = length(MARHD), drate = findrate(MARHD), mrate = findrate(MARHM))            
 
-df <- expand.grid(hour = c(12,1:11), period = c("am", "pm"))
-df <- cbind(df, rate_arr[2:4])
+hour_sex_marry <- ggplot(rate_arr, aes(x = factor(JWAP_h), y = mrate, fill=SEX)) +
+  geom_bar(stat = "identity", position = "dodge")+  
+  coord_polar(theta = "x", start = -0.13)+
+  xlab("Hour arrival at work") + ylab("Marriage rate")+
+  scale_fill_discrete(labels=c("Male", "Female"))+
+  theme(legend.justification=c(1,0), legend.position=c(1,0))
+
+hour_sex_marry  
+  
+hour_sex_divorce <- ggplot(rate_arr, aes(x = factor(JWAP_h), y = drate, fill=SEX)) +
+  geom_bar(stat = "identity", position = "dodge")+ 
+  coord_polar(theta = "x", start = -0.13)+
+  xlab("Hour arrival at work") + ylab("Divorce rate")+
+  scale_fill_discrete(labels=c("Male", "Female"))+
+  theme(legend.justification=c(1,0), legend.position=c(1,0))
+
+hour_sex_divorce
+
+###income
+
+Tuse$earning <- ifelse(Tuse$PERNP %in% c(-9000:0), 0, 
+                ifelse(Tuse$PERNP %in% c(0:104000), round(Tuse$PERNP/10000), 
+                ifelse(Tuse$PERNP %in% c(104000:504000), "10-50", "50+")))
+Tuse$earning <- factor(Tuse$earning, levels=c(as.character(0:10), "10-50", "50+"))
+
+rate_earn <- ddply(Tuse, .(earning, SEX), summarize, 
+                  n = length(MARHD), drate = findrate(MARHD), mrate = findrate(MARHM))
 
 
-hour_plot <- ggplot(df, aes(x = factor(hour), y = mrate, fill = period)) +
-  geom_bar(stat = "identity", position = "dodge")
-hour_plot
+earn_sex_marry <- ggplot(rate_earn, aes(x = as.factor(earning), y = mrate, 
+                                          group = SEX, fill=SEX)) +
+  geom_bar(stat = "identity", position = "dodge")+ 
+  xlab("Total person's earnings (in 10k dollars)") + ylab("Marriage rate")+
+  scale_fill_discrete(labels=c("Male", "Female"))+
+  theme(legend.justification=c(1,1), legend.position=c(1,1))
+earn_sex_marry
 
-hour_plot <- hour_plot + coord_polar(theta = "x", start = 0.26)
-hour_plot
+earn_sex_divorce <- ggplot(rate_earn, aes(x = as.factor(earning), y = drate, 
+                                          group = SEX, fill=SEX)) +
+  geom_bar(stat = "identity", position = "dodge")+ 
+  xlab("Total person's earnings (in 10k dollars)") + ylab("Divorce rate")+
+  scale_fill_discrete(labels=c("Male", "Female"))+
+  theme(legend.justification=c(1,1), legend.position=c(1,1))
+earn_sex_divorce
+  
+rate_earn_transfer <- rbind(data.frame(type="marry", rate_earn[, c(1:3)], rate=rate_earn[,5]),
+                            data.frame(type="divorce", rate_earn[, c(1:3)], rate=-rate_earn[,4]))
 
-hour_plot <- hour_plot + coord_polar(theta = "x", start = 0.26)+
-  xlab("")+ylab("")+
-  theme(axis.ticks = element_blank(), axis.text.y = element_blank(), 
-        panel.background = element_blank(), panel.grid.major.x = element_line(colour="grey"),
-        axis.text.x = element_text(size = 25), legend.title=element_blank())
-hour_plot
-
-hour_plot <- ggplot(rate_arr, aes(x = factor(JWAP_h), y = mrate, fill=as.factor(SEX))) +
-  geom_bar(stat = "identity", position = "dodge")+ coord_polar(theta = "x", start = -0.15)
-hour_plot
-
-
+earn_sex <- ggplot(rate_earn_transfer, 
+  aes(x = earning, y = rate, group = SEX, fill = SEX)) +
+  geom_bar(stat = "identity", position = "dodge")+ 
+  geom_hline(yintercept = 0)+
+  xlab("Total person's earnings (in 10k dollars)") + ylab("Marriage and Divorce rate")+
+  scale_fill_discrete(labels=c("Male", "Female"))+
+  theme(legend.justification=c(1,1), legend.position=c(1,1))
+earn_sex
