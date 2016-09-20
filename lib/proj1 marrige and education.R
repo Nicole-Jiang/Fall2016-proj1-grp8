f0259b5eaaf93af6b4b7f677c36afb6b27038599
Nicole Jiang <cj2451@columbia.edu>
2016-09-20 08:45:03 -0400

    change the divorce percentage of different groups

--- lib/proj1 marrige and education.R
+++ lib/proj1 marrige and education.R
@@ -61,7 +61,7 @@ library(stringr)
 
 edu= subset(data.fm, select= c("DIV","SCHL","SCIENGRLP"))
 edu[,1]= sapply(edu[,1], paste, "time(s)")
-edu_test= edu[1:50000,] ### 数据量太大，先跑50000行，之后再说吧
+edu_test= edu ### 数据量太大，先跑50000行，之后再说吧
 for(i in 1:nrow(edu_test)){
   if(edu_test[i,2]<15){
     edu_test[i,2]="Lowedu"
@@ -73,6 +73,9 @@ for(i in 1:nrow(edu_test)){
     edu_test[i,2]="Master"
   }else{
     edu_test[i,2]="PhD"
+  }
+  if(i%%10000==0){
+    print(i)
   }
 }
 
